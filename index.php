da41afc3a3db72fe9489c9f61d3695d14549e6ed
fitukek1 <tuomas.kekkonen@fi.abb.com>
2020-10-22 21:51:52 +0300

    improve style, order files by modified

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 if(isset($_GET['page']))
 {
 	$onlyfile = basename($_GET['page']);
-	include("pages/".filter_var($onlyfile, FILTER_SANITIZE_STRING));
+	$path="pages/".filter_var($onlyfile, FILTER_SANITIZE_STRING);
+	error_log($path);	
+	if(file_exists($path))
+	{
+		include($path);
+	}
 	exit();
 }
 echo "<!DOCTYPE html>\n<html>\n<head>\n";
@@ -12,18 +17,46 @@ include ("header.html");
 echo "</head>\n";
 echo "<body>\n";
 echo "<div id=\"main\">\n<div id=\"navigation\">";
-$files = glob('pages/*.md');
+//$files = glob('pages/*.md');
+$files = listdir_by_date('pages/', ".md");
 foreach($files as $filename)
 {
 	$justfilename = basename($filename);
-	$displayfilename = substr($justfilename, 0, -3);
+	$displayfilename = str_replace("_", " ", substr($justfilename, 0, -3));
 	echo "\n<a class=\"pagelinks\" href=# onClick=\"getMarkDown('".$justfilename."')\">".$displayfilename."</a><br>"; 
 }
-echo "\n</div>\n</div>\n";
+echo "\n</div>\n";
 include("body.html");
+echo "</div>";
 echo "<footer>";
 include("footer.html");
 echo "</footer>\n";
 echo "</body>\n";
 echo "</html>";
+
+
+
+function listdir_by_date($path, $filetype){
+    $dir = opendir($path);
+    $list = array();
+    while($file = readdir($dir)){
+        if (endsWith($file, $filetype) and $file != '.' and $file != '..'){
+            $ctime = filemtime($path . $file) . ',' . $file;
+            $list[$ctime] = $file;
+        }
+    }
+    closedir($dir);
+    krsort($list);
+    return $list;
+}
+
+function endsWith($string, $endString) 
+{ 
+  $len = strlen($endString); 
+  if ($len == 0) { 
+    return true; 
+  } 
+  return (substr($string, -$len) === $endString); 
+} 
+
 ?>
